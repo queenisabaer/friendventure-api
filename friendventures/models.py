5652b1970c3fe446260be9b3e06d6360b75df6f6
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-15 11:54:34 +0000

    Alter field time on friendventure model and create list view

--- friendventures/models.py
+++ friendventures/models.py
@@ -13,7 +13,7 @@ class Friendventure(models.Model):
     image = models.ImageField(
         upload_to='images/', default="../Friendventure_default_swdkd8")
     date = models.DateField()
-    time = models.DateTimeField()
+    time = models.TimeField()
     place = models.TextField()
     description = models.TextField(blank=True)
     category = models.CharField(
@@ -22,8 +22,14 @@ class Friendventure(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def get_datetime(self):
+        """Combine date and time to return a datetime object."""
+        from datetime import datetime
+        return datetime.combine(self.date, self.time)
     class Meta:
         ordering = ['-created_at', 'category', 'date']
 
     def __str__(self):
         return f'{self.id} {self.title} by {self.owner}'
+
+    
--- friendventures/views.py
+++ friendventures/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Friendventure
+from .serializers import FriendventureSerializer
 
-# Create your views here.
+class FriendventureList(APIView):
+    serializer_class=FriendventureSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    def get(self, request):
+        post = Friendventure.objects.all()
+        serializer = FriendventureSerializer(post, many= True, context={'request': request})
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = FriendventureSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
