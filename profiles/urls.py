578fbdc156c1ac3792e734431e5d13e2472422e3
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-15 07:03:49 +0000

    Add profiles detail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from profiles import views
 
 urlpatterns = [
     path("profiles/", views.ProfileList.as_view(), name="profiles"),
+    path("profiles/<int:pk>/", views.ProfileDetail.as_view(), name= "profile_detail")
 ]
 
