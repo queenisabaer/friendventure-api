21816ff8b054886e70765e12d5f0eb460a77c643
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-15 10:41:35 +0000

    Add friendventures app and create model

---
+++ friendventures/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+FRIENDVENTURE_CATEGORIES = (
+    ('Indoor', 'Indoor'),
+    ('Outdoor', 'Outdoor')
+)
+
+
+class Friendventure(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to='images/', default="../Friendventure_default_swdkd8")
+    date = models.DateField()
+    time = models.DateTimeField()
+    place = models.TextField()
+    description = models.TextField(blank=True)
+    category = models.CharField(
+        max_length=50, choices=FRIENDVENTURE_CATEGORIES, default='Outdoor'
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at', 'category', 'date']
+
+    def __str__(self):
+        return f'{self.id} {self.title} by {self.owner}'
--- fv_api/settings.py
+++ fv_api/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
     # Apps
     'profiles',
+    'friendventures',
 
    
 ]
