36a2c3b5963ddcc613b199ec9d625eebbb3f7945
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-15 12:11:11 +0000

    Add friendventure detail view

--- friendventures/urls.py
+++ friendventures/urls.py
@@ -3,6 +3,6 @@ from friendventures import views
 
 urlpatterns = [
     path("friendventures/", views.FriendventureList.as_view(), name="friendventure"),
-    # path("friendventures/<int:pk>/", views.FriendventureDetail.as_view(), name= "friendventure_detail")
+    path("friendventures/<int:pk>/", views.FriendventureDetail.as_view(), name= "friendventure_detail")
 ]
 
--- friendventures/views.py
+++ friendventures/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Friendventure
 from .serializers import FriendventureSerializer
+from fv_api.permissions import IsOwnerOrReadOnly
 
 class FriendventureList(APIView):
     serializer_class=FriendventureSerializer
@@ -23,4 +25,39 @@ class FriendventureList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+class FriendventureDetail(APIView):
+    serializer_class = FriendventureSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    def get_object(self, pk):
+        try:
+            friendventure = Friendventure.objects.get(pk=pk)
+            self.check_object_permissions(self.request, friendventure)
+            return friendventure
+        except Friendventure.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        friendventure = self.get_object(pk)
+        serializer = FriendventureSerializer(
+            friendventure, context={'request': request}
+            )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        friendventure = self.get_object(pk)
+        serializer = FriendventureSerializer(
+            friendventure, data=request.data, context={'request': request}
+            )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        friendventure = self.get_object(pk)
+        friendventure.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
