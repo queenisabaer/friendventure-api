e1d5ecbefd0c6fd55b7433e61bc4e54d217890a0
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-17 10:42:55 +0000

    Add serializer to participant

---
+++ participants/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Participant
+from django.db import IntegrityError
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
+    class Meta:
+        model = Participant
+        fields = [
+            'id',
+            'owner',
+            'friendventure',
+            'created_at'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You are already taking part in this friendventure.'
+            })
