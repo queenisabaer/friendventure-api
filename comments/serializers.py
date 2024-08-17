3d71fcf039f6da757a3dc3a0334332f748eb4f5e
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-17 06:05:56 +0000

    Create serializers for comment and comment detail

---
+++ comments/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import Comment
+from django.contrib.humanize.templatetags.humanize import naturaltime
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_pic = serializers.ReadOnlyField(
+        source='owner.profile.profile_image.url'
+    )
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'owner',
+            'is_owner',
+            'profile_id',
+            'profile_pic',
+            'created_at',
+            'updated_at',
+            'friendventure',
+            'content'
+        ]
+
+class CommentDetailSerializer(CommentSerializer):
+    friendventure = serializers.ReadOnlyField(source='friendventure.id')
