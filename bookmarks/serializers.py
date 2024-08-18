80ae906b5feeaa298cfc53dc8c5fef26292313a6
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-18 08:13:10 +0000

    feat: add serializer for bookmark model

---
+++ bookmarks/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Bookmark
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Bookmark model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Bookmark
+        fields = ['id', 'created_at', 'owner', 'friendventure']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'You have already bookmarked this friendventure.'
+            })
