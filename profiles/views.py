69cb03720782c203f81e3ad071773f3a0a4b80ad
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-19 11:14:35 +0000

    feat: add filter functionality to profile list view

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Profile
 from .serializers import ProfileSerializer
 from fv_api.permissions import IsOwnerOrReadOnly
@@ -16,8 +17,15 @@ class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
 
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
     ]
+
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed_by__owner__profile',
+    ]
+
     ordering_fields = [
         'friendventures_count',
         'followers_count',
