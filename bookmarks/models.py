7dd8ea0c712e69cf5f9288bd7a36a01039b53fe6
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-20 09:19:12 +0000

    style: add missing white lines, change line breaks in bookmarks app files

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -5,22 +5,27 @@ from friendventures.models import Friendventure
 
 class Bookmark(models.Model):
     """
-    A model representing a bookmark made by a user for a specific friendventure.
+    Model representing a bookmark made by a user for a specific friendventure.
 
     Attributes:
-    owner (User): A foreign key reference to the User model, indicating the user who created the bookmark.
-    friendventure (Friendventure): A foreign key reference to the Friendventure model, indicating the friendventure that has been bookmarked.
-    created_at (DateTimeField): A timestamp indicating when the bookmark was created. Automatically set to the current date and time when the bookmark is created.
+    owner (User): Foreign key reference to the User model, indicating the user
+     who created the bookmark.
+    friendventure (Friendventure): Foreign key reference to the FriendVenture-
+     model, indicating the friendventure that has been bookmarked.
+    created_at (DateTimeField): A timestamp indicating when the bookmark was
+     created. Automatically set to the current date and time when the bookmark
+     is created.
     """
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     friendventure = models.ForeignKey(
-        Friendventure, related_name='bookmarks', on_delete=models.CASCADE
+        Friendventure, related_name="bookmarks", on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
-        unique_together = ['owner', 'friendventure']
+        ordering = ["-created_at"]
+        unique_together = ["owner", "friendventure"]
 
     def __str__(self):
-        return f'{self.owner} {self.friendventure}'
+        return f"{self.owner} {self.friendventure}"
--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -15,11 +15,12 @@ class BookmarkListViewTest(APITestCase):
             password="password",
         )
         self.test_friendventure = Friendventure.objects.create(
-            owner=self.user, 
-            title='friendventure title', 
+            owner=self.user,
+            title='friendventure title',
             date='2024-08-29',
             time='14:00:00'
         )
+
     def test_can_list_bookmarks(self):
         response = self.client.get('/bookmarks/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -42,15 +43,18 @@ class BookmarkListViewTest(APITestCase):
                 }
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-    
+
     def test_cant_bookmark_a_friendventure_twice(self):
         self.client.login(username='testuser', password='password')
         user = User.objects.get(username='testuser')
-        friendventure = Friendventure.objects.get(id=self.test_friendventure.id)
+        friendventure = Friendventure.objects.get(
+            id=self.test_friendventure.id
+        )
         response = self.client.post(
             '/bookmarks/', {'owner': user, 'friendventure': friendventure}
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
 
 class BookmarkDetailViewTest(APITestCase):
     """
@@ -66,21 +70,21 @@ class BookmarkDetailViewTest(APITestCase):
             password="password2",
         )
         self.test_friendventure1 = Friendventure.objects.create(
-            owner= self.testuser1,
-            title = 'friendventure title testuser1',
-            date = '2024-09-01',
-            time ='15:00:00',
-            place = 'Test place testuser1',
-            description = 'Test description 1',
-            category = 'Indoor'
+            owner=self.testuser1,
+            title='friendventure title testuser1',
+            date='2024-09-01',
+            time='15:00:00',
+            place='Test place testuser1',
+            description='Test description 1',
+            category='Indoor'
         )
         self.test_friendventure2 = Friendventure.objects.create(
-            owner= self.testuser2,
-            title = 'friendventure title testuser2',
-            date = '2024-09-03',
-            time ='09:00:00',
-            place = 'Test place testuser2',
-            description = 'Test description 2',
+            owner=self.testuser2,
+            title='friendventure title testuser2',
+            date='2024-09-03',
+            time='09:00:00',
+            place='Test place testuser2',
+            description='Test description 2',
         )
         self.test_bookmark = Bookmark.objects.create(
             owner=self.testuser2,
