a62e885683b04fc93c823918d81f0d452a6901d6
Dajana Isbaner <dajanaisbaner@gmx.de>
2024-08-23 08:13:39 +0000

    fix: update fv_api views to use rest auth dictionary

--- fv_api/views.py
+++ fv_api/views.py
@@ -1,9 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .settings import (
-    JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWT_AUTH_SAMESITE,
-    JWT_AUTH_SECURE,
-)
+from django.conf import settings 
 
 
 @api_view()
@@ -20,23 +17,28 @@ def root_route(request):
 # dj-rest-auth logout view fix
 @api_view(['POST'])
 def logout_route(request):
+    jwt_auth_cookie = settings.REST_AUTH.get('JWT_AUTH_COOKIE')
+    jwt_auth_refresh_cookie = settings.REST_AUTH.get('JWT_AUTH_REFRESH_COOKIE')
+    jwt_auth_samesite = settings.REST_AUTH.get('JWT_AUTH_SAMESITE')
+    jwt_auth_secure = settings.REST_AUTH.get('JWT_AUTH_SECURE')
+
     response = Response()
     response.set_cookie(
-        key=JWT_AUTH_COOKIE,
+        key=jwt_auth_cookie,
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=jwt_auth_samesite,
+        secure=jwt_auth_secure,
     )
     response.set_cookie(
-        key=JWT_AUTH_REFRESH_COOKIE,
+        key=jwt_auth_refresh_cookie,
         value='',
         httponly=True,
         expires='Thu, 01 Jan 1970 00:00:00 GMT',
         max_age=0,
-        samesite=JWT_AUTH_SAMESITE,
-        secure=JWT_AUTH_SECURE,
+        samesite=jwt_auth_samesite,
+        secure=jwt_auth_secure,
     )
     return response
